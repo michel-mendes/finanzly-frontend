330092809d8d47f13597c2bec87c227ea9e0100a
Michel Mendes <michel.gmsm@gmail.com>
2023-04-22 11:57:31 -0300

    chore: add some more implementations in transactions page

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -3,26 +3,45 @@ import { useNavigate } from "react-router-dom"
 import { IoArrowBack } from "react-icons/io5"
 import { RiArrowDropDownFill } from "react-icons/ri"
 
+// Authentication context
+import { useAuthContext } from "../../contexts/Auth"
+
 // Hooks
 import { useWallets } from "../../hooks/useWallets"
 import { useCategories } from "../../hooks/useCategories"
 import { useTransactions } from "../../hooks/useTransactions"
+import { useModal } from "../../hooks/useModal"
 
 // Components
 import { PageHeaderDesktop } from "../../shared_components/PageHeaderDesktop"
 import { WalletSelector } from "./WalletSelector"
+import { ModalSaveCancel } from "../../shared_components/Modal"
 
 // Interfaces
 import { ITransaction, IWallet } from "../../services/types"
 
 import styles from "./styles.module.css"
+import { FormTransactionCRUD } from "../../shared_components/FormTransactionCRUD"
 
 function TransactionsPage() {
+    const {loggedUser} = useAuthContext()
     const navigate = useNavigate()
 
     const { walletsList } = useWallets()
     const { categoriesList } = useCategories()
-    const { transactionsList, getTransactionsFromWallet, clearTransactionsList } = useTransactions()
+    const {
+        transactionsList,
+        draftTransaction,
+        setDraftTransaction,
+        getTransactionsFromWallet,
+        newTransaction,
+        updateTransaction,
+        deleteTransaction,
+        clearTransactionsList
+    } = useTransactions()
+    
+    const { isOpen, showModal, closeModal } = useModal()
+    const [isEditing, setIsEditing] = useState(false)
     
     const [activeWallet, setActiveWallet] = useState<IWallet | null>(null)
 
@@ -31,6 +50,41 @@ function TransactionsPage() {
         (!activeWallet) ? clearTransactionsList() : getTransactionsFromWallet(activeWallet.id!)
 
     }, [activeWallet])
+
+    function handleNewTransactionClick() {
+        setIsEditing(false)
+        setDraftTransaction({
+            fromUser: loggedUser?.id,
+            fromWallet: activeWallet?.id,
+        })
+
+        showModal()
+    }
+
+    function handleListItemClick(transaction: ITransaction) {
+        setIsEditing(true)
+        setDraftTransaction(transaction)
+
+        showModal()
+    }
+    
+    async function handleSaveButtonClick() {
+        const success = (isEditing) ? await updateTransaction(draftTransaction!) : await newTransaction(draftTransaction!)
+
+        if (success) {
+            alert("Transação adicionada com sucesso")
+            closeModal()
+        }
+    }
+
+    async function handleDeleteButtonClick(transaction: ITransaction) {
+        const success = await deleteTransaction(transaction)
+
+        if (success) {
+            alert("Transação removida com sucesso")
+            closeModal()
+        }
+    }
 
     return (
         <div className={styles.page_container}>
@@ -46,20 +100,9 @@ function TransactionsPage() {
                         />
                     </div>
 
-                    <button>Nova transação</button>
+                    <button onClick={handleNewTransactionClick}>Nova transação</button>
                 </div>
             </PageHeaderDesktop>
-
-            <p>Carteiras</p>
-            {
-                walletsList.map(wallet => {
-                    return (
-                        <div key={wallet.id}>
-                            <p>{wallet.walletName}</p>
-                        </div>
-                    )
-                })
-            }
 
             <p>Transações</p>
             {
@@ -67,7 +110,7 @@ function TransactionsPage() {
                     const category = categoriesList.find(item => { return (item.id == transaction.fromCategory) })
 
                     return (
-                        <div key={transaction.id}>
+                        <div key={transaction.id} onClick={() => {handleListItemClick(transaction)}}>
                             <p>Transação número: {index + 1}</p>
                             <p>Data: {new Date(transaction.date!).toLocaleDateString()}</p>
                             <p>Descrição: {transaction.description}</p>
@@ -77,6 +120,21 @@ function TransactionsPage() {
                     )
                 })
             }
+
+            <ModalSaveCancel
+                isOpen={isOpen}
+                modalTitle="Nova transação" 
+                modalButtons={{
+                    saveButton: {onClick: handleSaveButtonClick},
+                    cancelButton: {onClick: closeModal},
+                    deleteButton: {
+                        onClick: () => {handleDeleteButtonClick(draftTransaction!)},
+                        enabled: isEditing
+                    }
+                }}
+            >
+                <FormTransactionCRUD transactionData={draftTransaction} setTransactionData={setDraftTransaction}/>
+            </ModalSaveCancel>
         </div>
     )
 }
