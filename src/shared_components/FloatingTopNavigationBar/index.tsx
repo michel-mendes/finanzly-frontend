fc7bf2dfb76668a72594c5ccdada591de8cbf1d8
Michel Mendes <michel.gmsm@gmail.com>
2023-08-31 13:16:10 -0300

    chore: adds a floating top nav bar

--- src/shared_components/FloatingTopNavigationBar/index.tsx
+++ src/shared_components/FloatingTopNavigationBar/index.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { FiChevronDown } from 'react-icons/fi'
+
+// Auth context
+import { useAuthContext } from '../../contexts/Auth'
 
 import styles from "./styles.module.css"
+import moment from 'moment'
 
 function FloatingTopNavigationBar() {
+    const { loggedUser } = useAuthContext()
+    const navigate = useNavigate()
+
+    function handleDashboardLinkClick() {
+        const start = moment(Date.now()).startOf('month').toISOString(true).split("T")[0]
+        const end = moment(Date.now()).endOf('month').toISOString(true).split("T")[0]
+        const activeWallet = (loggedUser?.activeWalletId) ? `&wallet=${loggedUser.activeWalletId}` : ``
+
+        navigate(`/dashboard?start=${start}&end=${end}${activeWallet}`)
+    }
+
     return (
         <nav className={styles.navbar}>
             <div className={styles.logo_container}>
@@ -10,17 +26,20 @@ function FloatingTopNavigationBar() {
             </div>
 
             <ul className={styles.links_container}>
-                <li>Dashboard</li>
-                <li>Carteiras</li>
-                <li>Categorias</li>
-                <li>Transações</li>
+                <li onClick={handleDashboardLinkClick}>Dashboard</li>
+                <li onClick={() => { navigate("/wallets") }}>Carteiras</li>
+                <li onClick={() => { navigate("/categories") }}>Categorias</li>
+                <li onClick={() => { navigate("/transactions") }}>Transações</li>
             </ul>
 
             <div className={styles.user_container}>
                 <img className={styles.user_icon} src="https://e7.pngegg.com/pngimages/550/997/png-clipart-user-icon-foreigners-avatar-child-face.png" alt="" />
 
-                <span>Michel Mendes</span>
-                <span>V</span>
+                <span>{loggedUser?.firstName}</span>
+
+                <span className={styles.user_drop_down_arrow}>
+                    <FiChevronDown />
+                </span>
             </div>
         </nav>
     )
