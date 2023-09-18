2559b3f81410e16932a0a15480e48459f85c88cf
Michel Mendes <michel.gmsm@gmail.com>
2023-09-18 01:01:03 -0300

    chore: changes the loading effect component

--- src/shared_components/LoadingPageOverlay/index.tsx
+++ src/shared_components/LoadingPageOverlay/index.tsx
@@ -3,8 +3,8 @@ import style from "./styles.module.css"
 function LoadingOverlay() {
 
     return (
-      <div className={ style.overlay_loading }>
-        <h2>Loading...</h2>
+      <div className={style.loader_container}>
+        <div className={style.custom_loader}></div>
       </div>
     )
 }
