5931ed33ec0f391647e364cd10bb2a3a49a817ec
Michel Mendes <michel.gmsm@gmail.com>
2023-10-11 00:43:51 -0300

    chore: creates props definition module "prop-defs"

--- src/prop-defs.tsx
+++ src/prop-defs.tsx
@@ -1,6 +1,13 @@
+import { Dispatch, SetStateAction } from "react"
+
 export interface ITransactionsPageFilters {
     text: string,
     category: string,
     startDate: Date,
     endDate: Date
+}
+
+export interface ITransactionSearchBoxProps {
+    filters: ITransactionsPageFilters,
+    setFilters: Dispatch<SetStateAction<ITransactionsPageFilters>>
 }
