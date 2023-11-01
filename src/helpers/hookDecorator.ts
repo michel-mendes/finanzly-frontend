b1c968cc5a589515430a8729be0bae38988e9187
michel-mendes <michel.gmsm@gmail.com>
2023-11-01 00:53:11 -0300

    fix: corrects the function return in order to work

--- src/helpers/hookDecorator.ts
+++ src/helpers/hookDecorator.ts
@@ -1,12 +1,12 @@
 // Exceute this decorator on every decorated function call
-export function decorateCustomHookFunction<F extends Function>(func: F, setLoading: (isLoading: boolean) => void): F {
+export function decorateCustomHookFunction<F extends (...args: any[]) => Promise<any>>(func: F, setLoading: (isLoading: boolean) => void): F {
     
     // Before the function call sets the loading state "true"
     // After the function conclusion sets the loading state "false"
-    return ( (...args: any[]) => {
+    return ( async (...args: Parameters<F>) => {
         setLoading(true)
-        
-        func(...args).finally(() => {
+
+        return func(...args).finally(() => {
             setLoading(false)
         })
     }) as unknown as F
