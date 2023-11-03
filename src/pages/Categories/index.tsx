a9d90ce6ec148655fc1a53284e09d9c7420354b4
Michel Mendes <michel.gmsm@gmail.com>
2023-11-03 11:07:52 -0300

    chore: adapts CategoriesPage to UseCategories hook

--- src/pages/Categories/index.tsx
+++ src/pages/Categories/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { IoArrowBack } from "react-icons/io5"
 
@@ -10,52 +10,40 @@ import { FormCategoryCRUD } from "../../components/FormCategoryCRUD"
 
 // Hooks
 import { useModal } from "../../hooks/useModal"
+import { useToastNotification } from "../../hooks/useToastNotification"
 
 // Components
 import { PageHeaderDesktop } from "../../components/PageHeaderDesktop"
 import { CategoriesList } from "./CategoriesList"
 import { ModalSaveCancel } from "../../components/Modal"
+import { LoadingOverlay } from "../../components/LoadingPageOverlay"
 
 // Services
 import { useAuthContext } from "../../contexts/Auth"
-import { CategoriesApi } from "../../services/CategoriesApi"
+import { useCategories } from "../../hooks/useCategories"
 
 import styles from "./styles.module.css"
 
 function CategoriesPage() {
     const { loggedUser } = useAuthContext()
     const navigate = useNavigate()
-    const api = new CategoriesApi()
+
+    const {showSuccessNotification} = useToastNotification()
+
+    const {categoriesList, loadingCategories, awaitingResponse, tempCategory, setTempCategory, createCategory, updateCategory, deleteCategory} = useCategories()
 
     const { isOpen, showModal, closeModal } = useModal()
 
-    const [modalCategory, setModalCategory] = useState<ICategory | null>(null)
     const [modalTitle, setModalTitle] = useState("")
     const [isEditing, setIsEditing] = useState(false)
 
-    const [categoriesList, setCategoriesList] = useState<ICategory[]>([])
-
-    useEffect(() => {
-        async function getMyCategories() {
-            const list = await api.getCategoriesFromLoggedUser()
-
-            if ("error" in list) {
-                alert(`Erro ao atualizar lista de categorias:\n\n${list.error}`)
-                return
-            }
-
-            setCategoriesList(list)
-        }
-
-        getMyCategories()
-    }, [])
 
     function handleOpenModal(categoryToEdit: ICategory | null = null) {
         setIsEditing(categoryToEdit !== null)
         setModalTitle((categoryToEdit !== null) ? "Alterar categoria" : "Nova categoria")
         
         if (!categoryToEdit) {
-            setModalCategory({
+            setTempCategory({
                 categoryName: "",
                 fromUser: loggedUser?.id,
                 iconPath: "",
@@ -63,60 +51,34 @@ function CategoriesPage() {
             })
         }
         else {
-            setModalCategory(categoryToEdit)
+            setTempCategory(categoryToEdit)
         }
 
         showModal()
     }
 
     async function handleModalSaveClick() {
-        const savedCategory = (!isEditing) ? (await api.createCategory({...modalCategory!, fromUser: loggedUser?.id})) : (await api.updateCategory(modalCategory!.id!, modalCategory!))
+        const success = (!isEditing) ? (await createCategory({...tempCategory!, fromUser: loggedUser?.id})) : (await updateCategory(tempCategory!.id!, tempCategory!))
 
-        if ("error" in savedCategory) {
-            alert(`Erro durante a operação:\n${savedCategory.error}`)
-            console.log(savedCategory)
-            return
+        if (success) {
+            setTempCategory(null)
+            closeModal()
         }
-
-        if (isEditing) {
-            const updatedCategoriesList = categoriesList!.map(category => {
-                return (savedCategory.id == category.id) ? savedCategory : category
-            })
-
-            setCategoriesList(updatedCategoriesList)
-        }
-        else {
-            setCategoriesList([...categoriesList!, savedCategory])
-        }
-
-        setModalCategory(null)
-        closeModal();
     }
 
     function handleModalCancelClick() {
-        setModalCategory(null)
-        closeModal();
+        setTempCategory(null)
+        closeModal()
     }
 
     async function handleModalDeleteClick() {
-        const deletedCategory = await api.deleteCategory(modalCategory?.id!)
+        const success = await deleteCategory(tempCategory?.id!)
 
-        if ("error" in deletedCategory) {
-            alert(`Erro durante a operação:\n${deletedCategory.error}`)
-            console.log(deletedCategory)
-            return
+        if (success) {
+            showSuccessNotification(`A categoria "${tempCategory?.categoryName}" foi removida`)
+            setTempCategory(null)
+            closeModal()
         }
-
-        const newList = categoriesList.filter(category => {
-            if (category.id !== deletedCategory.id) { return category }
-        })
-
-        setCategoriesList(newList)
-
-        alert("Categoria excluída com sucesso!")
-
-        setModalCategory(null)
-        closeModal();
     }
 
     return (
@@ -130,7 +92,9 @@ function CategoriesPage() {
             </PageHeaderDesktop>
 
             <div className={styles.list_container}>
-                <CategoriesList categoriesList={categoriesList} onClickItem={handleOpenModal} />
+                {
+                    loadingCategories ? <LoadingOverlay /> : <CategoriesList categoriesList={categoriesList} onClickItem={handleOpenModal} />
+                }
             </div>
 
             <ModalSaveCancel isOpen={isOpen} modalTitle={modalTitle} modalButtons={{
@@ -138,7 +102,7 @@ function CategoriesPage() {
                 cancelButton: { onClick: handleModalCancelClick },
                 deleteButton: { onClick: handleModalDeleteClick, enabled: isEditing }
             }}>
-                <FormCategoryCRUD categoryData={modalCategory} setCategoryData={setModalCategory} />
+                <FormCategoryCRUD categoryData={tempCategory} setCategoryData={setTempCategory} />
             </ModalSaveCancel>
         </div>
     )
