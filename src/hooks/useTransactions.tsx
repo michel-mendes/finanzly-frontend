cf2fb6dfc586b59df3b8fcdf218cce306d1e0c6b
Michel Mendes <michel.gmsm@gmail.com>
2023-11-06 15:07:43 -0300

    chore: centers TransactonsApi into useTransactions

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,89 +1,127 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
+import moment from "moment"
 
 // Services
-import { TransactionsApi } from "../services/TransactionsApi"
+import { api } from "../helpers/apiCall"
+import { handleError } from "../helpers/errorHandler"
 
 // Interfaces
-import { ITransaction } from "../services/types"
+import { ITransaction } from "../type-defs"
+
+// Hooks
+import { useToastNotification } from "./useToastNotification"
+
+// Custom decorator
+import { decorateCustomHookFunction } from "../helpers/hookDecorator"
+
+// Configs
+import { appConfigs } from "../../config/app-configs"
+
+
+const {
+    transactionGetFromWallet,
+    transactionCreateEndpoint,
+    transactionUpdateEndpoint,
+    transactionDeleteEndpoint
+} = appConfigs
 
 export function useTransactions() {
-    const api = new TransactionsApi()
 
-    const [draftTransaction, setDraftTransaction] = useState<ITransaction | null>(null)
+    const {showErrorNotification} = useToastNotification()
+
+    const [tempTransaction, setTempTransaction] = useState<ITransaction | null>(null)
     const [transactionsList, setTransactionsList] = useState<ITransaction[]>([])
     const [loadingTransactions, setLoadingTransactions] = useState(true)
+    const [awaitingResponse, setAwaitingResponse] = useState(false)
 
-    async function getTransactionsFromWallet(walletId: string, startDate: string = "", endDate: string = "") {
-        setLoadingTransactions(true)
+    // Keeps the lastest value of wallet balance
+    const [walletBalanceAfterLastTransaction, setWalletBalanceAfterLastTransaction] = useState(0)
 
-        const walletTransactions = await api.getTransactionsFromWallet(walletId, startDate, endDate)
-
-        if ("error" in walletTransactions) {
-            alert(`Erro ao atualizar transções da carteira:\n\n${walletTransactions.error}`)
-            return
-        }
-
-        setTransactionsList(walletTransactions)
-        setLoadingTransactions(false)
-    }
-
-    function clearTransactionsList() {
+    
+    function clearList() {
         setTransactionsList([])
     }
+    
+    async function getTransactionsFromWallet(walletId: string, startDate: string = "", endDate: string = "") {
+        try {
+            startDate = (startDate.length > 0) ? `startDate=${startDate}&` : ""
+            endDate = (endDate.length > 0) ? `endDate=${endDate}&` : ""
+            const queryString = (startDate || endDate) ? `?${startDate}${endDate}` : ""
+            
+            const transactions: ITransaction[] = (await api.get(`${transactionGetFromWallet}${walletId}${queryString}`)).data
 
-    async function newTransaction(data: ITransaction): Promise<ITransaction | null> {
-        const newTransaction = await api.createTransaction(data)
-
-        if ("error" in newTransaction) {
-            alert(`Erro ao inserir nova transação:\n\n${newTransaction.error}`)
-            return null
+            setTransactionsList(transactions)
+        } catch (error: any) {
+            showErrorNotification(handleError(error).message)
         }
-
-        setTransactionsList([...transactionsList, newTransaction])
-        return newTransaction
     }
 
-    async function updateTransaction(data: ITransaction): Promise<ITransaction | null> {
-        const updatedTransaction = await api.updateTransaction(data.id!, data)
+    async function createTransaction(transactionData: ITransaction) {
+        try {
+            transactionData.date = moment(transactionData.date).startOf("day") as any //get timestamp
+            const createdTransaction: ITransaction = (await api.post(transactionCreateEndpoint, transactionData)).data
 
-        if ("error" in updatedTransaction) {
-            alert(`Erro ao editar transação:\n\n${updatedTransaction.error}`)
-            return null
+            setTransactionsList([...transactionsList, createdTransaction])
+            setWalletBalanceAfterLastTransaction(createdTransaction.currentWalletBalance!)
+            return true
+        } catch (error: any) {
+            showErrorNotification( handleError(error).message )
+            return false
         }
-
-        const updatedTransactionsList = transactionsList.map(transaction => {
-            return (transaction.id == updatedTransaction.id) ? updatedTransaction : transaction
-        })
-
-        setTransactionsList(updatedTransactionsList)        
-        return updatedTransaction
     }
 
-    async function deleteTransaction(transaction: ITransaction): Promise<ITransaction | null> {
-        const deletedTransaction = await api.deleteTransaction(transaction.id!)
+    async function updateTransaction(transactionId: string, transactionData: ITransaction) {
+        try {
+            // id, fromUser and actualBalance can't be sent
+            // other properties are destructured in "newData"
+            const { id, fromUser, fromWallet, csvImportId, creditValue, debitValue, ...newData } = transactionData
 
-        if ("error" in deletedTransaction) {
-            alert(`Erro ao deletar transação:\n\n${deletedTransaction.error}`)
-            return null
+            const updatedTransaction: ITransaction = (await api.put(`${transactionUpdateEndpoint}${transactionId}`, newData)).data
+
+            const updatedTransactionsList = transactionsList.map(transaction => {
+                return (transaction.id == updatedTransaction.id) ? updatedTransaction : transaction
+            })
+
+            setTransactionsList(updatedTransactionsList)
+            setWalletBalanceAfterLastTransaction(updatedTransaction.currentWalletBalance!)
+            return true
+        } catch (error: any) {
+            showErrorNotification( handleError(error).message )
+            return false
         }
-
-        const updatedTransactionsList = transactionsList.filter(transaction => {
-            return (transaction.id !== deletedTransaction.id)
-        })
-
-        setTransactionsList(updatedTransactionsList)
-        return deletedTransaction
     }
+
+    async function deleteTransaction(transactionId: string) {
+        try {
+            const deletedTransaction: ITransaction = (await api.delete(`${transactionDeleteEndpoint}${transactionId}`)).data
+
+            const updatedTransactionsList = transactionsList.filter(transaction => {
+                return transaction.id !== deletedTransaction.id
+            })
+
+            setTransactionsList(updatedTransactionsList)
+            setWalletBalanceAfterLastTransaction(deletedTransaction.currentWalletBalance!)
+            return true
+        } catch (error: any) {
+            showErrorNotification( handleError(error).message )
+            return false
+        }
+    }
+
+    const decoratedGetTransactionsFromWallet = decorateCustomHookFunction(getTransactionsFromWallet, setLoadingTransactions)
+    const decoratedCreateTransaction = decorateCustomHookFunction(createTransaction, setAwaitingResponse)
+    const decoratedUpdateTransaction = decorateCustomHookFunction(updateTransaction, setAwaitingResponse)
+    const decoratedDeleteTransaction = decorateCustomHookFunction(deleteTransaction, setAwaitingResponse)
 
     return {
-        draftTransaction, setDraftTransaction,
-        transactionsList, setTransactionsList,
-        loadingTransactions, setLoadingTransactions,
-        
-        getTransactionsFromWallet,
-        newTransaction,
-        updateTransaction,
-        deleteTransaction,
-        clearTransactionsList
+        tempTransaction, setTempTransaction,
+        transactionsList, clearList,
+        getTransactionsFromWallet: decoratedGetTransactionsFromWallet,
+        createTransaction: decoratedCreateTransaction,
+        updateTransaction: decoratedUpdateTransaction,
+        deleteTransaction: decoratedDeleteTransaction,
+        walletBalanceAfterLastTransaction,
+        loadingTransactions,
+        awaitingResponse
     }
 }
