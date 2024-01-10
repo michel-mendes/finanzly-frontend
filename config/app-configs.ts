9a164a58f98b636a0aebdb944d61e7bd25471435
Michel Mendes <michel.gmsm@gmail.com>
2024-01-10 01:25:44 -0300

    chore: reafctors app-configs module

--- config/app-configs.ts
+++ config/app-configs.ts
@@ -3,40 +3,40 @@ const baseApiUrl = import.meta.env.VITE_API_BASE_URL
 export const appConfigs = {
     
     // Users endpoints
-    userEditEndpoint: baseApiUrl + import.meta.env.VITE_USER_PUT,
-    userLoginEnpoint: baseApiUrl + import.meta.env.VITE_USER_LOGIN_ENDPOINT,
-    userLogoutEnpoint: baseApiUrl + import.meta.env.VITE_USER_LOGOUT_ENDPOINT,
-    getUserLoggedEndpoint: baseApiUrl + import.meta.env.VITE_GET_LOGGED_USER_ENDPOINT,
-    userSetActiveWalletEndpoint: baseApiUrl + import.meta.env.VITE_USER_SET_ACTIVE_WALLET,
+    userEditEndpoint: `${baseApiUrl}/users/`, // + userId
+    userLoginEnpoint: `${baseApiUrl}/users/authenticate`,
+    userLogoutEnpoint: `${baseApiUrl}/users/logout`,
+    getUserLoggedEndpoint: `${baseApiUrl}/users/current`,
+    userSetActiveWalletEndpoint: `${baseApiUrl}/users/set-active-wallet`,
 
     // Wallets endpoints
-    walletCreateEndpoint: `${baseApiUrl}${import.meta.env.VITE_WALLET_POST}`,
-    walletGetAllEndpoint: `${baseApiUrl}${import.meta.env.VITE_WALLET_GET_ALL}`,
-    walletGetByIdEndpoint: `${baseApiUrl}${import.meta.env.VITE_WALLET_GET_BY_ID}`,
-    walletGetFromUserEndpoint: `${baseApiUrl}${import.meta.env.VITE_WALLET_GET_FROM_USER}`,
-    walletUpdateEndpoint: `${baseApiUrl}${import.meta.env.VITE_WALLET_PUT}`,
-    walletDeleteEndpoint: `${baseApiUrl}${import.meta.env.VITE_WALLET_DELETE}`,
-    walletCalculateBalanceEndpoint: `${baseApiUrl}${import.meta.env.VITE_WALLET_CALCULATE_BALANCE}`,
+    walletCreateEndpoint: `${baseApiUrl}/wallets/`,
+    walletGetAllEndpoint: `${baseApiUrl}/wallets/`,
+    walletGetByIdEndpoint: `${baseApiUrl}/wallets/`, // + walletId
+    walletGetFromUserEndpoint: `${baseApiUrl}/wallets/from-user/`,
+    walletUpdateEndpoint: `${baseApiUrl}/wallets/`, // + walletId
+    walletDeleteEndpoint: `${baseApiUrl}/wallets/`, // + walletId
+    walletCalculateBalanceEndpoint: `${baseApiUrl}/wallets/calculate-balance`,
 
     // Categories endpoints
-    categoryCreateEndpoint: `${baseApiUrl}${import.meta.env.VITE_CATEGORY_POST}`,
-    categoryGetAllEndpoint: `${baseApiUrl}${import.meta.env.VITE_CATEGORY_GET_ALL}`,
-    categoryGetByIdEndpoint: `${baseApiUrl}${import.meta.env.VITE_CATEGORY_GET_BY_ID}`,
-    categoryGetCategoriesFromUser: `${baseApiUrl}${import.meta.env.VITE_CATEGORY_GET_FROM_USER}`,
-    categoryUpdateEndpoint: `${baseApiUrl}${import.meta.env.VITE_CATEGORY_PUT}`,
-    categoryDeleteEndpoint: `${baseApiUrl}${import.meta.env.VITE_CATEGORY_DELETE}`,
+    categoryCreateEndpoint: `${baseApiUrl}/categories/`,
+    categoryGetAllEndpoint: `${baseApiUrl}/categories/`,
+    categoryGetByIdEndpoint: `${baseApiUrl}/categories/`, // + categoryId
+    categoryGetCategoriesFromUser: `${baseApiUrl}/categories/from-user/`,
+    categoryUpdateEndpoint: `${baseApiUrl}/categories/`, // + categoryId
+    categoryDeleteEndpoint: `${baseApiUrl}/categories/`, // + categoryId
 
     // Transactions Endpoints
-    transactionCreateEndpoint: `${baseApiUrl}${import.meta.env.VITE_TRANSACTION_POST}`,
-    transactionGetAllEndpoint: `${baseApiUrl}${import.meta.env.VITE_TRANSACTION_GET_ALL}`,
-    transactionGetByIdEndpoint: `${baseApiUrl}${import.meta.env.VITE_TRANSACTION_GET_BY_ID}`,
-    transactionGetFromWallet: `${baseApiUrl}${import.meta.env.VITE_TRANSACTION_GET_FROM_WALLET}`,
-    transactionUpdateEndpoint: `${baseApiUrl}${import.meta.env.VITE_TRANSACTION_PUT}`,
-    transactionDeleteEndpoint: `${baseApiUrl}${import.meta.env.VITE_TRANSACTION_DELETE}`,
+    transactionCreateEndpoint: `${baseApiUrl}/transactions/`,
+    transactionGetAllEndpoint: `${baseApiUrl}/transactions/`,
+    transactionGetByIdEndpoint: `${baseApiUrl}/transactions/`, // + transactionId
+    transactionGetFromWallet: `${baseApiUrl}/transactions/from-wallet/`,
+    transactionUpdateEndpoint: `${baseApiUrl}/transactions/`, // + transactionId
+    transactionDeleteEndpoint: `${baseApiUrl}/transactions/`, // + transactionId
     
     // Import Endpoint
-    importPostEndpoint: `${baseApiUrl}${import.meta.env.VITE_IMPORT_POST}`,
+    importPostEndpoint: `${baseApiUrl}/upload/`,
 
     // Report Endpoint
-    reportGetEndpoint: `${baseApiUrl}${import.meta.env.VITE_REPORT_GET}`
+    reportGetEndpoint: `${baseApiUrl}/report/`
 }
